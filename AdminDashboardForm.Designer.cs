7eb5f67ff2b3cfc77b02b8a71ef29b77333c2ebd
code-oshi123 <ushaniprarthana593@gmail.com>
2025-05-28 11:28:18 +0530

    uploaded files again

---
+++ AdminDashboardForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Dbex
+{
+    partial class AdminDashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            linkLabel3 = new LinkLabel();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(330, 110);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(109, 20);
+            linkLabel1.TabIndex = 0;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "1.Add Location";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Location = new Point(327, 162);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(98, 20);
+            linkLabel2.TabIndex = 1;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "2.View Events";
+            linkLabel2.LinkClicked += linkLabel2_LinkClicked;
+            // 
+            // linkLabel3
+            // 
+            linkLabel3.AutoSize = true;
+            linkLabel3.Location = new Point(327, 212);
+            linkLabel3.Name = "linkLabel3";
+            linkLabel3.Size = new Size(179, 20);
+            linkLabel3.TabIndex = 2;
+            linkLabel3.TabStop = true;
+            linkLabel3.Text = "3.View Sales and Revenue";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(327, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(134, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Admin Dash Board";
+            // 
+            // AdminDashboardForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(linkLabel3);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Name = "AdminDashboardForm";
+            Text = "AdminDashboardForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+        private LinkLabel linkLabel3;
+        private Label label1;
+    }
+}
---
+++ AdminevviewForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Dbex
+{
+    partial class AdminevviewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            adminevviewlistBox = new ListBox();
+            vieweventadminbackbutton = new Button();
+            vieweventsadminbutton = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(352, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 20);
+            label1.TabIndex = 0;
+            label1.Text = "View Events";
+            // 
+            // adminevviewlistBox
+            // 
+            adminevviewlistBox.FormattingEnabled = true;
+            adminevviewlistBox.Location = new Point(161, 103);
+            adminevviewlistBox.Name = "adminevviewlistBox";
+            adminevviewlistBox.Size = new Size(537, 244);
+            adminevviewlistBox.TabIndex = 1;
+            // 
+            // vieweventadminbackbutton
+            // 
+            vieweventadminbackbutton.Location = new Point(534, 380);
+            vieweventadminbackbutton.Name = "vieweventadminbackbutton";
+            vieweventadminbackbutton.Size = new Size(216, 29);
+            vieweventadminbackbutton.TabIndex = 2;
+            vieweventadminbackbutton.Text = "Back to Add Event Form";
+            vieweventadminbackbutton.UseVisualStyleBackColor = true;
+            vieweventadminbackbutton.Click += vieweventadminbackbutton_Click;
+            // 
+            // vieweventsadminbutton
+            // 
+            vieweventsadminbutton.Location = new Point(177, 380);
+            vieweventsadminbutton.Name = "vieweventsadminbutton";
+            vieweventsadminbutton.Size = new Size(140, 29);
+            vieweventsadminbutton.TabIndex = 3;
+            vieweventsadminbutton.Text = "View Events";
+            vieweventsadminbutton.UseVisualStyleBackColor = true;
+            vieweventsadminbutton.Click += vieweventsadminbutton_Click;
+            // 
+            // AdminevviewForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(vieweventsadminbutton);
+            Controls.Add(vieweventadminbackbutton);
+            Controls.Add(adminevviewlistBox);
+            Controls.Add(label1);
+            Name = "AdminevviewForm";
+            Text = "AdminevviewForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox adminevviewlistBox;
+        private Button vieweventadminbackbutton;
+        private Button vieweventsadminbutton;
+    }
+}
